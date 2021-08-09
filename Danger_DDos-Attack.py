a6ef92233534378e5701ec4a5649b7c4bf066251
Garuda12cyber <88545901+Garuda12cyber@users.noreply.github.com>
2021-08-09 21:13:09 +0700

    Update Danger_DDos-Attack.py

--- Danger_DDos-Attack.py
+++ Danger_DDos-Attack.py
@@ -2,10 +2,6 @@ import time
 import socket
 import random
 import sys
-
-os.system("clear") 
-os.system("termux-open-url https://youtube.com/channel/uc5qattgpidzabvoskwkcytq")
-
 def usage():
     print "\033[1;31m                      ..-------.`"
     print "\033[1;31m                `--::-..........-:::--`"
